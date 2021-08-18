898a55e05ff7f280c183da0955b74ffdfde2766c
Pravesh <praveshdwivedi999@gmail.com>
2021-08-18 18:07:14 +0530

    updated

--- index.js
+++ index.js
@@ -2,51 +2,74 @@ require("dotenv").config();
 const app = require("express")();
 const axios = require("axios");
 const cors = require("cors");
-const TwitterClient = require("twitter-api-client").TwitterClient;
 const port = process.env.PORT || 5000;
+var Twit = require('twit');  
+var Twitter = require('twitter');
 
-const twitterClient = new TwitterClient({
-  apiKey: process.env.TWITTER_API_KEY,
-  apiSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
 
 app.use(cors({ origin: "*" }));
 
 app.get("/", function (req, res) {
   res.send("Welcome to the twitter and instagram api API");
 });
-
-app.get("/twitter/:query", function (req, res) {
-  const query = req.params.query;
-  twitterClient.tweets
-    .search({
-      q: query,
-    })
-    .then((response) => {
-      res.send(response);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("An error occured. Please try again letter");
-    });
+// var T = new Twit({
+//   consumer_key: process.env.TWITTER_API_KEY ,
+//   consumer_secret: process.env.TWITTER_API_SECRET,
+//   access_token: process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+//   timeout_ms:  60*1000 
+// });
+// T.get(
+//   "search/tweets",
+//   { q: "givingtuesdayca ", count: 30, result_type: "recent" },
+//   function (err, data, response) {
+//     console.log(data);
+//   }
+// );
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-app.get("/instagram/:query", function (req, res) {
-  const query = req.params.query;
-
-  axios
-    .get("https://www.instagram.com/explore/tags/" + query + "/?__a=1")
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("An error occurred, please try again later.");
-    });
+app.get('/tweet', function(req, res, next) {
+  client.get('statuses/user_timeline', { screen_name: 'GivingTuesdayCa', count: 30, result_type:'recent' }, function(error, tweets, response) {
+    if (!error) {
+      var result=[];
+     tweets.map(data=>{
+       var newdata=data.entities.urls
+       newdata.map(list=>{
+          var tweetData={
+            text:data.text,
+            expanded_url:list.expanded_url
+          }
+          result.push(tweetData);
+       })
+     })
+     res.send({
+       status:true,
+      data:result
+      })
+      
+    }
+    else {
+      res.status(500).json({ error: error });
+    }
+  });
 });
 
 app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
 });
+
+// exports.getTweets = async (req, res) => {
+//   T.get(
+//     "search/tweets",
+//     { q: "givingtuesdayca ", count: 10, result_type: "recent" },
+//     function (err, data, response) {
+//       console.log(data);
+//       res.send(data);
+//     }
+//   );
+// };
